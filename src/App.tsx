2a6c856fa1193040666fa38f8d2b214df0c53feb
Oluwataye <davidtayeibukunoni@gmail.com>
2026-01-25 15:38:43 +0100

    feat(auth): remove session idle timeout and warning component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
-import { SessionTimeoutWarning } from "./components/auth/SessionTimeoutWarning";
 import { OfflineProvider } from "./contexts/OfflineContext";
 import { InventoryProvider } from "./contexts/InventoryContext";
 import DashboardLayout from "./components/layout/DashboardLayout";
@@ -63,7 +62,6 @@ const App = ({ queryClient, persister }: AppProps) => {
       <OfflineProvider>
         <AuthProvider>
           <InventoryProvider>
-            <SessionTimeoutWarning />
             <TooltipProvider>
               <Toaster />
               <Sonner />
